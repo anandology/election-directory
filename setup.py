3c1f17fbe1d448fc2963b2a91725d94c210fe742
Anand Chitipothu <anandology@gmail.com>
2015-03-25 19:19:18 +0530

    Converted election_directory module into a package.
    This makes distributing the data easier.

--- setup.py
+++ setup.py
@@ -4,9 +4,11 @@ setup(
     name='election-directory',
     version='0.1.0',
     description='Library to provide information about assembly constituencies and polling booths of India',
-    py_modules=['election_directory'],
+    packages=['election_directory'],
     install_requires=[],
     author='Anand Chitipothu',
     author_email="anandology@gmail.com",
     platforms='any',
+    package_data={'election_directory': ['data/*.tsv']}
 )
+
